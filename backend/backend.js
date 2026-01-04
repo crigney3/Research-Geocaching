ac59d6017f1deb63bdd175648fc40035d83f7cbf
Corey Rigney <coreyrigney@gmail.com>
2026-01-03 17:23:02 -0800

    syntax

--- backend/backend.js
+++ backend/backend.js
@@ -169,12 +169,12 @@ const handleXPLevelAndRange = async (id, updatedStat, oldStatValue, newStatValue
         // We need to update xp, level, and range
         let calculatedRange = 100 + 50 * Math.log2(levelResults[1] + 1);
         await pool.query("UPDATE achievements SET level=?, xp=?, userRange=? WHERE id=?", [levelResults[1], levelResults[2], calculatedRange, id]);
-        const [row] = await pool.query("SELECT * FROM achievements WHERE id=?", [id]);              
+        const row = await pool.query("SELECT * FROM achievements WHERE id=?", [id]);              
         return {error: false, leveled: false, user: row[0]};
     } else {
         // We only need to update internal xp
         await pool.query("UPDATE achievements SET xp=xp+? WHERE id=?", [xpToAdd, id]);
-        const [row] = await pool.query("SELECT * FROM achievements WHERE id=?", [id]);              
+        const row = await pool.query("SELECT * FROM achievements WHERE id=?", [id]);              
         return {error: false, leveled: false, user: row[0]};
     }
 }
