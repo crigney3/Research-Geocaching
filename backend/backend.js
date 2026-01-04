41c7248a4a38e1d6f3652124d7f9d1d8aef94da9
Corey Rigney <coreyrigney@gmail.com>
2026-01-03 16:17:30 -0800

    typo

--- backend/backend.js
+++ backend/backend.js
@@ -169,7 +169,7 @@ const handleXPLevelAndRange = async (id, updatedStat, oldStatValue, newStatValue
     if (levelResults[0]) {
         // We need to update xp, level, and range
         let calculatedRange = 100 + 50 * Math.log2(levelResults[1] + 1);
-        await pool.query("UPDATE achievements SET level=?, xp=?, userRange=? WHERE id=?", [levelResults[1], levelResults[2], calculatedRange, inID],
+        await pool.query("UPDATE achievements SET level=?, xp=?, userRange=? WHERE id=?", [levelResults[1], levelResults[2], calculatedRange, id],
             async function(err, row) {
                 if (err) {
                     console.error("Error getting user: %s", err);
@@ -181,7 +181,7 @@ const handleXPLevelAndRange = async (id, updatedStat, oldStatValue, newStatValue
         );
     } else {
         // We only need to update internal xp
-        await pool.query("UPDATE achievements SET xp=xp+? WHERE id=?", [xpToAdd, inID],
+        await pool.query("UPDATE achievements SET xp=xp+? WHERE id=?", [xpToAdd, id],
             async function(err, row) {
                 if (err) {
                     console.error("Error getting user: %s", err);
