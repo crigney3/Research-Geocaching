3e1796e9ca1ac6c5266ba5a0fc6e0929b7b569e7
Corey Rigney <coreyrigney@gmail.com>
2026-01-08 15:54:21 -0800

    A refactor, fixedtm

--- backend/backend.js
+++ backend/backend.js
@@ -576,7 +576,9 @@ app.post('/google_login', authenticateToken, async (req, res) => {
             
             // Fetch the newly created user
             const [newUserRows] = await promiseQuery("SELECT * FROM users WHERE id=?", [client_id]);
-            user = newUserRows;
+            user = newUserRows[0];
+            console.log(user);
+            console.log(newUserRows);
             msg = "Authentication Successful for new user";
             
             if (!user || !user.length) {
@@ -592,7 +594,7 @@ app.post('/google_login', authenticateToken, async (req, res) => {
             
             // Fetch achievements
             const [achievementRows] = await promiseQuery("SELECT * FROM achievements WHERE id=?", [client_id]);
-            achievements = achievementRows;
+            achievements = achievementRows[0];
         }
         
         console.log("Fetched user: " + user);
